c1940f7a9553e38a7910ca308bd7366f581598f5
Sahil Paul <46472937+SahilPaul88@users.noreply.github.com>
2019-02-07 12:22:42 +0530

    Add files via upload

---
+++ my-cal/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,46 @@
+package com.mycompany.Calculator;
+
+
+public class App
+{
+	public static void main(String args[])
+	{
+		int res,a,b,choice;
+		
+		
+		 a=Integer.parseInt(args[0]);
+		 b=Integer.parseInt(args[1]);
+		 choice=Integer.parseInt(args[2]);		
+		
+			System.out.println("1.Multiplication");
+			System.out.println("2.Add");
+			System.out.println("3.Subtract");
+			System.out.println("4.exit");
+			System.out.println("enter your choice");
+			
+			switch(choice)
+			{
+				case 1: res=a*b;
+				System.out.println("product is"+res);
+				break;
+ 			
+				case 2:
+				res=a+b;
+				System.out.println("sum is"+res);
+				break;	
+				
+				case 3:
+				res=a-b;
+				System.out.println("difference is"+res);
+				break;
+				case 4: System.exit(0);
+					break;
+				default: System.out.println("invalid choice");
+
+			}
+			
+	}
+}
+
+
+
